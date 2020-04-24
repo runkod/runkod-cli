c941b6c81390a7a8ba09ee81ca13b59a9a4c3de6
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-04-24 08:55:46 +0300

    list command code fix & reformat

--- src/commands/list.js
+++ src/commands/list.js
@@ -5,28 +5,28 @@ import {_t} from '../i18n';
 import {projectFormatter, SEPARATOR} from '../formatter.js';
 
 module.exports = async (self, config) => {
-  const resp = await config.api.getProjects();
+  const show = (projects) => {
+    if (projects.length === 0) {
+      log.bold(_t('list.no-projects'));
+      log.info(_t('list.no-projects-hint'));
+      return;
+    }
 
-  if (resp.code) {
-    log.error(resp.message);
-    return;
+    let screen = '';
+    const countText = projects.length === 1 ? _t('list.count-label-single') : _t('list.count-label', {n: projects.length});
+    screen += chalk.inverse.bold(countText) + '\n';
+
+    for (let item of projects) {
+      screen += projectFormatter(item) + '\n';
+    }
+
+    screen += SEPARATOR;
+
+    console.log(screen);
+  };
+
+  const projects = await config.api.getProjects();
+  if (projects) {
+    show(projects);
   }
-
-  if (resp.length === 0) {
-    log.bold(_t('list.no-projects'));
-    log.info(_t('list.no-projects-hint'));
-    return;
-  }
-
-  let screen = '';
-  const countText = resp.length === 1 ? _t('list.count-label-single') : _t('list.count-label', {n: resp.length});
-  screen += chalk.inverse.bold(countText) + '\n';
-
-  for (let item of resp) {
-    screen += projectFormatter(item) + '\n';
-  }
-
-  screen += SEPARATOR;
-
-  console.log(screen);
 };
